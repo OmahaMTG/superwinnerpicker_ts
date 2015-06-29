f14608906a1772ba4a4f5c081282cacff77608ad
Brian Olson <beolson@gmail.com>
2015-06-28 21:55:12 -0500

    begin work on winner Name display

---
+++ src/app/winnerName.ts
@@ -0,0 +1,9 @@
+///<reference path="../../tools/typings/tsd.d.ts" />
+///<reference path="../../tools/typings/typescriptApp.d.ts" />
+class WinnerName {
+	constructor(platformHeight: number, game: Phaser.Game, winnerName: string ){
+		
+	}
+	
+	
+}
